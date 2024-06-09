e2cae5634c5d827d0aef893c9c2afc6177e91d04
Alexandros Chatzis <alexpaschalischatzis@gmail.com>
2024-06-09 12:26:10 +0300

    CH12 Homework Exercise

---
+++ src/gr/aueb/cf/ch12/Main.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.ch12;
+
+import gr.aueb.cf.ch12.model.User;
+import gr.aueb.cf.ch12.model.UserCredentials;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User(120030104, "Alex", "Chatzis");
+
+        UserCredentials userCredentials = new UserCredentials(123012052, "Christina", "K.");
+
+        System.out.println("ID: " + user.getId());
+        System.out.println("First Name: " + user.getFirstname());
+        System.out.println("Last Name: " + user.getLastname());
+
+        System.out.println("ID: " + userCredentials.getId());
+        System.out.println("First Name: " + userCredentials.getFirstname());
+        System.out.println("Last Name: " + userCredentials.getLastname());
+
+
+    }
+}
